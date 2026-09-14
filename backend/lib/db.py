d4191e0a999ab2158d54bd0b06648ef7b6fd8422
Halil Tuna <halilkocabiyikci@gmail.com>
2026-09-14 13:08:06 -0600

    update db.py

--- backend/lib/db.py
+++ backend/lib/db.py
@@ -10,7 +10,7 @@ from pymongo import ASCENDING, DESCENDING, IndexModel
 
 load_dotenv(Path(__file__).parent.parent / ".env")
 
-mongo_url = os.environ[mongodb+srv://gridline-admin:SOMEPASSWORD@cluster0.something.mongodb.net/?retryWrites=true&w=majority]
+mongo_url = os.environ[mongodb+srv://halilkocabiyikci_db_user:Sz0xfGZf3SpFcjBm@gridline-cluster.dnhklhw.mongodb.net]
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ["DB_NAME"]]
 
